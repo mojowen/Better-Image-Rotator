bd98f6591b04837763114db7a6129f5c35953863
Scott Duncombe <srduncombe@gmail.com>
2012-03-23 01:01:34 -0400

    Admin JS file

---
+++ better_image_rotator_admin.js
@@ -0,0 +1,22 @@
+jQuery(document).ready( function($) {
+	$( "#sortable" ).sortable({ items: "li:not(:last)" });
+	$( "#sortable" ).disableSelection();
+
+	$('#sortable li a.delete').live('click', function(e) { e.preventDefault(); 
+		if( $(this).parents('ul').find('li:not(:last)').length == 1 ) {
+			$(this).parent('li').find('input[type=text]').val('');
+			$(this).parent('li').find('.preview').remove();
+		} else {
+			$(this).parent('li').remove(); 
+		}
+	});
+
+	$('#sortable li a.add').live('click', function(e) { 
+		e.preventDefault(); 
+		var elem = $(this).parents('ul').find('li:first').clone();
+		$('input[type=text]',elem).val('');
+		$('.preview',elem).remove();
+		$(elem).insertBefore( $(this).parents('li') );
+	});
+
+});
